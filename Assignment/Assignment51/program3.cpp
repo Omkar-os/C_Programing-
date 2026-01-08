94f4d107ec4cb5343caa06a4b049452ff716b247
Omkar-os <omkarsudrik22@gmail.com>
2026-01-08 14:46:00 +0530

    Add files via upload

---
+++ Assignment/Assignment51/program3.cpp
@@ -0,0 +1,31 @@
+//Write a generic program which accepts one value and one number from user and print that value that number of times.
+#include<iostream>
+using namespace std;
+
+template <class T>
+int  SearchFirst(T *arr, int iSize,T iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        
+            if(arr[iCnt] == iNo)
+            {
+               return iCnt;
+            }
+            
+    }
+    return -1;
+}          
+
+int main()
+{
+    int arr[]={10,20,30,10,30,40,10,40,10};
+    
+    int iRet = SearchFirst(arr,9,40);
+
+    cout<<iRet;
+
+    return 0;
+}
